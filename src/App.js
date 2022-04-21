07c296f884e221e6fca00753539ea8fb02858429
Adenusi Adetayo <adenusiadetayo@gmail.com>
2022-04-21 15:22:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+let total = ' ';
 function App() {
+  const [screen, setScreen] = useState('0');
+  const handleClick = (val) => {
+    total += val;
+    setScreen(total);
+  };
+
+  const handleEquals = () => {
+    setScreen(eval(total));
+    total = ' ';
+  };
+
+  const handleClear = () => {
+    setScreen('0');
+    total = ' ';
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>calculator</h1>
+      <div class="container">
+        <span>{screen}</span>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('/')}>/</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={handleClear}>c</button>
+        <button onClick={handleEquals}>=</button>
+      </div>
     </div>
   );
 }
